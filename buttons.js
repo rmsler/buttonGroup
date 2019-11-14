5e3e314682ebccbdbc7dbd4b8890fb89eedda7d7
Leon Robert <ler@rmsiasi.onmicrosoft.com>
2019-11-14 12:01:05 +0200

    group buttons that can change the behavior of buttons using mode

--- buttons.js
+++ buttons.js
@@ -9,20 +9,43 @@ var Button = function(config) {
         this.state = !this.state;
     };
 };
-var buttons= new Array();
+var GroupButton = function(array, mode){
+    this.array = array;
+    this.changeMode = () =>{
+        $.each(this.array, function (index, value){
+            value.state = false;
+        });
+        return this.array;
+    }
+    this.clickButton = (index) =>{
+        if(this.array[index] === false){
+            if(mode !== 'checkboxes'){
+                $.each(array, function (counter,value){
+                    value.state = false
+                });
+            }
+        }
+        this.array[index].state = !this.array[index].state;
+        return this.array;
+    }
+}
+var group;
 var userdata = $.getJSON("config.json", function (data) {
     values = data["buttons"];
+    let buttons = new Array();
     $.each(values, function (index, value){
         buttons[index] = new Button(value);
         console.log(buttons[index]);
     });
+    let state = $(".button-type").text().toLowerCase();
+    group = new GroupButton(buttons, state);
+    console.log(buttons);
+    console.log(typeof(buttons));
 });
-console.log(buttons);
-console.log(typeof(buttons));
 $(document).ready(function(){
     $(".button-type").click(function(){
         $(this).text() === 'Radio buttons' ? 
-        ($(this).text('Checkboxes'), $(this).removeClass("radios").addClass("checkboxes")) :  
+        ($(this).text('Checkboxes'), $(this).removeClass("radios").addClass("checkboxes"), group.changeMode()) :  
         ($(this).text('Radio buttons'),$(this).removeClass("checkboxes").addClass("radios"))
     });
 });
