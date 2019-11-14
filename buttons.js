fd3b5f196c126595f018b432e3af99bc2f5822f4
Leon Robert <ler@rmsiasi.onmicrosoft.com>
2019-11-14 11:04:39 +0200

    small change

--- buttons.js
+++ buttons.js
@@ -1,17 +1,20 @@
 var Button = function(config) {
+    if (!(this instanceof Button)) { 
+        return new Button(config);
+      }
     this.name = config.Name || "button";
     this.state = config.Selected ;
     this.class = config.CustomClass
     this.onClick = () => {
-        this.state === true ? false : true
+        this.state = !this.state;
     };
 };
 var buttons= new Array();
 var userdata = $.getJSON("config.json", function (data) {
     values = data["buttons"];
     $.each(values, function (index, value){
-        buttons[index] = (new Button(value));
-        console.log(buttons[0]);
+        buttons[index] = new Button(value);
+        console.log(buttons[index]);
     });
 });
 console.log(buttons);
