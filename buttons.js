509a6aa63c30b9dbc52f6f2a2e23a9577ef501d9
Leon Robert <ler@rmsiasi.onmicrosoft.com>
2019-11-14 15:28:52 +0200

    small change

--- buttons.js
+++ buttons.js
@@ -1,20 +1,8 @@
-// var Button = function(config, changeStateCallback) {
-//     if (!(this instanceof Button)) { 
-//         return new Button(config, changeStateCallback);
-//     }
-//     this.name = config.Name || "button";
-//     this.state = config.Selected ;
-//     this.class = config.CustomClass;
-//     this.changeStateCallback = changeStateCallback;
-//     this.onClick = () => {
-//         this.state = !this.state;
-//         this.changeStateCallback();
-//     };
-//     this.init = () =>{};
-// };
-import {Button} from "./modules/Button.js/index.js"
-import {GroupButton} from "./modules/GroupButton.js/index.js"
+import {Button} from "./modules/Button.js"
+import {GroupButton} from "./modules/GroupButton.js"
+
 var group;
+
 var userdata = $.getJSON("config.json", (data) => {
     let values = data["buttons"];
     let buttons = new Array();
